01e603c0a8279082284a65e1f86ad88912d3f66f
Vlad Pomazan <vladpomazan22@gmail.com>
2020-07-04 17:17:10 +0300

    add seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Model\User\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -21,7 +21,8 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'status' => random_int(1, 2) == 1 ? User::STATUS_WAIT :  User::STATUS_ACTIVE,
+        'verify_token' => Str::uuid(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
